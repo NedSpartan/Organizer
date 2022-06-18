52b34ec04580cddf8b25b583058a33d1a70c1dca
NedSpartan <46873713+NedSpartan@users.noreply.github.com>
2022-06-18 01:53:24 -0400

    Add files via upload

--- org.py
+++ org.py
@@ -1,18 +1,86 @@
 import os
 import shutil
 
+from sys import platform
+
 class Mover:
 
     def __init__(self):
         
         # Source Folder to watch
-        self.source = os.listdir(f'/home/{os.getlogin()}/Downloads')
 
-        # Windows downloads path --> C:\Users\YourUserName\Downloads
+        self.source = ''
+        
+        # Checks to see what os is running and sets the path appropriately
+
+        if platform == "linux":
+
+            self.source = os.listdir(f'/home/{os.getlogin()}/Downloads')
+
+            paths = [
+                'Pictures','Movies','Documents','Games'
+            ]
+
+            count = 0
+
+            for i in paths:
+
+                if os.path.exists(f'/home/{os.getlogin()}/Downloads/{paths[count]}') == True:
+
+                    count += 1
+
+                else:
+
+                    os.mkdir(f'/home/{os.getlogin()}/Downloads/{paths[count]}')
+
+                    count += 1
+
+        elif platform == 'win32':
+
+            self.source = os.listdir(f'C:\\Users\\{os.getlogin()}\\Downloads\\')
+
+            paths = [
+                'Pictures','Movies','Documents','Games'
+            ]
+
+            count = 0
+
+            for i in paths:
+
+                if os.path.exists(f'C:\\Users\\{os.getlogin()}\\Downloads\\{paths[count]}') == True:
+
+                    count += 1
+
+                else:
+
+                    os.mkdir(f'C:\\Users\\{os.getlogin()}\\Downloads\\{paths[count]}')
+
+                    count += 1
+
+        else:
+
+            self.source = os.listdir(f'/home/{os.getlogin()}/Downloads')
+
+            paths = [
+                'Pictures','Movies','Documents','Games'
+            ]
+
+            count = 0
+
+            for i in paths:
+
+                if os.path.exists(f'/home/{os.getlogin()}/Downloads/{paths[count]}') == True:
+
+                    count += 1
+
+                else:
+
+                    os.mkdir(f'/home/{os.getlogin()}/Downloads/{paths[count]}')
+
+                    count += 1
+
 
         # List of possible destinations
-        self.destinations = [os.listdir('/home/chris/Downloads/Pictures'),os.listdir('/home/chris/Downloads/Movies'),
-        os.listdir('/home/chris/Downloads/Games')]
 
         self.jpg = 0
         self.zip = 0
@@ -22,26 +90,52 @@ class Mover:
         self.file_totals = 0
 
     def mover(self):
+        
         count = 0
 
         for i in self.source:
+
             result = self.source[count].split('.')
+
             # Remove before production prints the split results of all the files in the directory
+            
             print(result[-1])
+
+            def osChecker(fileType):
+
+                if platform == 'linux':
+
+                    source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
+                    destination = f'/home/{os.getlogin()}/Downloads/{fileType}/{self.source[count]}'
+
+                    shutil.move(source, destination)
+
+                elif platform == 'win32':
+
+                    source = f'C:\\Users\\{os.getlogin()}\\Downloads\\{self.source[count]}'
+                    destination = f'C:\\Users\\{os.getlogin()}\\Downloads\\{fileType}\\{self.source[count]}'
+
+                    shutil.move(source, destination)
+
+                else:
+
+                    source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
+                    destination = f'/home/{os.getlogin()}/Downloads/{fileType}/{self.source[count]}'
+
+                    shutil.move(source, destination)
 
             if result[-1] == 'jpg':
 
                 self.jpg += 1
                 self.file_totals += 1
                 
-                source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
-                destination = f'/home/{os.getlogin()}/Downloads/Pictures/{self.source[count]}'
+                osChecker('Pictures')
 
-                shutil.move(source, destination)
+                count += 1
 
-                #Remove before production
-                print(source)
-                print(destination)
+            elif result[-1] == 'jpeg':
+
+                osChecker('Pictures')
 
                 count += 1
 
@@ -50,10 +144,7 @@ class Mover:
                 self.zip += 1
                 self.file_totals += 1
 
-                source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
-                destination = f'/home/{os.getlogin()}/Downloads/Games/{self.source[count]}'
-
-                shutil.move(source, destination)
+                osChecker('Games')
                 
                 count += 1
 
@@ -62,10 +153,7 @@ class Mover:
                 self.rar += 1
                 self.file_totals += 1
 
-                source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
-                destination = f'/home/{os.getlogin()}/Downloads/Games/{self.source[count]}'
-
-                shutil.move(source, destination)
+                osChecker('Games')
                 
                 count += 1
 
@@ -74,10 +162,7 @@ class Mover:
                 self.iso += 1
                 self.file_totals += 1
 
-                source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
-                destination = f'/home/{os.getlogin()}/Downloads/Games/{self.source[count]}'
-
-                shutil.move(source, destination)
+                osChecker('Games')
 
                 count += 1
 
@@ -86,10 +171,31 @@ class Mover:
                 self.png += 1
                 self.file_totals += 1
 
-                source = f'/home/{os.getlogin()}/Downloads/{self.source[count]}'
-                destination = f'/home/{os.getlogin()}/Downloads/Pictures/{self.source[count]}'
+                osChecker('Pictures')
 
-                shutil.move(source, destination)
+                count += 1
+
+            elif result[-1] == 'csv':
+
+                osChecker('Documents')
+
+                count += 1
+
+            elif result[-1] == 'SRT':
+
+                osChecker('Movies')
+
+                count += 1
+
+            elif result[-1] == 'AC3-EVO':
+
+                osChecker('Movies')
+
+                count += 1
+
+            elif result[-1] == 'XviD-EVO':
+
+                osChecker('Movies')
 
                 count += 1
 
@@ -99,12 +205,9 @@ class Mover:
 
 # Remove before production
 test = Mover()
-print(test.mover())
+test.mover()
 
 print(test.source)
-print(test.destinations[0])
-print(test.destinations[1])
-print(test.destinations[2])
 print(test.jpg)
 print(test.zip)
 print(test.rar)
